914f37f94dba6dc228838dd6d91dc5442486ed4f
Anthony <gachinigaithuma@gmail.com>
2024-09-16 14:12:34 +0300

    done

--- server/app.py
+++ server/app.py
@@ -35,7 +35,7 @@ class Plants(Resource):
         db.session.add(new_plant)
         db.session.commit()
 
-        return make_response(new_plant.to_dict(), 201)
+        return make_response(jsonify(new_plant.to_dict()), 201)
 
 
 api.add_resource(Plants, '/plants')
@@ -44,8 +44,33 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        return make_response(jsonify({"error": "Plant not found"}), 404)
+
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        data = request.get_json()
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        db.session.commit()
+
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response("", 204)
+
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response("", 204)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
